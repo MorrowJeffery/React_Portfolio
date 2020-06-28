b810b7b05bf5b149b03d9c1333e5a4dcde576503
Jeff Morrow <morrowjefferya@gmail.com>
2020-06-27 22:04:21 -0700

    Updates

---
+++ precache-manifest.762d22f049cc01259ec0cd4f849e9b8e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64cec9f3a0072b3a7d0bd69f1aed7992",
+    "url": "/React_Portfolio/index.html"
+  },
+  {
+    "revision": "51db71f3c685b19509c1",
+    "url": "/React_Portfolio/static/css/main.d46f5b5d.chunk.css"
+  },
+  {
+    "revision": "742e1ebfb22befd41f15",
+    "url": "/React_Portfolio/static/js/2.3af37fe9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React_Portfolio/static/js/2.3af37fe9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51db71f3c685b19509c1",
+    "url": "/React_Portfolio/static/js/main.74adaa11.chunk.js"
+  },
+  {
+    "revision": "c083a7718907dd4b36b5",
+    "url": "/React_Portfolio/static/js/runtime-main.73c16ebd.js"
+  },
+  {
+    "revision": "ed38981a3540fabc36d4537bbe4e5bdb",
+    "url": "/React_Portfolio/static/media/DateNightlogo.ed38981a.PNG"
+  },
+  {
+    "revision": "d75131e5b0a75229e4390861e8e93837",
+    "url": "/React_Portfolio/static/media/ProfPic.d75131e5.png"
+  },
+  {
+    "revision": "4897b1587851b264f67beca752a4572d",
+    "url": "/React_Portfolio/static/media/Resume.4897b158.pdf"
+  },
+  {
+    "revision": "04dc4b8e8c676201e0fee809e6519c9c",
+    "url": "/React_Portfolio/static/media/TrivialCalendarApp.04dc4b8e.PNG"
+  },
+  {
+    "revision": "16f982b1e632c3d510bd73bf6f091d3d",
+    "url": "/React_Portfolio/static/media/burgerApp.16f982b1.PNG"
+  },
+  {
+    "revision": "a7005c464b7a51199099aad47761ce6c",
+    "url": "/React_Portfolio/static/media/onIceApp.a7005c46.PNG"
+  }
+]);
